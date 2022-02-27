2d39104838fce8bec016dd3ea473a0e969b01f35
Filip Alexandrov <filipalexandrov@gmail.com>
2022-02-27 10:03:06 +0100

    firebase auth with google

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,1 +1,16 @@
+<script>
+	import { auth, googleProvider } from '../firebase';
+	import { signInWithPopup } from 'firebase/auth';
+
+	const login = async () => {
+		try {
+			const res = await signInWithPopup(auth, googleProvider);
+			console.log(res.user);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+</script>
+
 <h1>Login</h1>
+<button on:click={login}>Login using Google</button>
