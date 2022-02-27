0363737c1a3845533ecb93288b0699f8c2c7d710
Filip Alexandrov <filipalexandrov@gmail.com>
2022-02-27 09:42:30 +0100

    initial html structure

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,38 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/profile">Profile</a>
+		</li>
+		<li>
+			<a href="/login">Login</a>
+		</li>
+		<li>
+			<a href="/">Logout</a>
+		</li>
+	</ul>
+</nav>
+
+<slot />
+
+<style>
+	ul {
+		display: flex;
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+	li {
+		margin-right: 20px;
+		font-weight: bold;
+	}
+	a {
+		color: #555;
+		text-decoration: none;
+	}
+	a:hover {
+		color: green;
+	}
+</style>
